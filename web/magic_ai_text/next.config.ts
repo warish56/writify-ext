c8fc38e08910bf1e8cf2c4cd7692a62f67f22a63
Md Warish Ali Ansari <mdwarish56@gmail.com>
2024-12-28 17:05:44 +0530

    next config changes (#22)
    * next config changes
    
    * image updated
    
    * impor tpaths modified
    
    * next config changes
    
    * image updated
    
    * next config changes
    
    * next config changes
    
    ---------
    
    Co-authored-by: warish-56 <warish@grexit.com>

--- web/magic_ai_text/next.config.ts
+++ web/magic_ai_text/next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'standalone',
+  reactStrictMode: true,
+  images: {
+    localPatterns: [
+      {
+        pathname: 'public/asset/**',
+        search: '',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
