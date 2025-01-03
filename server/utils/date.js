d357ce4ee6364cdb0d54e6dc05979c660f1403e3
warish-56 <warish@grexit.com>
2025-01-03 09:58:35 +0530

    date bug fixed

--- server/utils/date.js
+++ server/utils/date.js
@@ -4,8 +4,8 @@ const isNextDay = (timestamp1, timestamp2) => {
     const d1 = new Date(timestamp1);
     const d2 = new Date(timestamp2);
 
-    const startDate = new Date(d1.getFullYear(), d1.getMonth(), d1.getDay());
-    const endDate = new Date(d2.getFullYear(), d2.getMonth(), d2.getDay());
+    const startDate = new Date(d1.getFullYear(), d1.getMonth(), d1.getDate());
+    const endDate = new Date(d2.getFullYear(), d2.getMonth(), d2.getDate());
 
     return endDate > startDate;
 
